627fd690a293498b30bbe1bb4f4d108093bace35
J.D. Bean <jonathandbean@gmail.com>
2018-02-19 17:34:53 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -36,6 +36,8 @@ def shortened_tweet_truncator(tweet)
   if tweet.length > 140
     if word_substituter(tweet).length > 140
       word_substituter(tweet)[0.140] << "..."
+    else word_substituter(tweet)
+    end
   else
     tweet
   end
