28e8e6ddbd698d99f56ef3ca7e56eb261a72891c
J.D. Bean <jonathandbean@gmail.com>
2018-02-19 17:13:57 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,8 +17,7 @@ end
 
 def word_substituter(tweet)
   tweet_text = tweet.split(" ")
-     puts  tweet_text.collect { |x| dictionary.keys.include?(x.to_sym) ? dictionary[x.to_sym] : x }.join(" ")
-  binding.pry
+  tweet_text.collect { |x| dictionary.keys.include?(x.to_sym) ? dictionary[x.to_sym] : x }.join(" ")
 end
 
 word_substituter("Hey buddy, how are you")
