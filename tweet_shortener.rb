69167e288dabc646a79da3712848d755d208c20c
J.D. Bean <jonathandbean@gmail.com>
2018-02-19 17:12:34 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,7 @@ end
 
 def word_substituter(tweet)
   tweet_text = tweet.split(" ")
-      puts tweet_text.collect { |x| dictionary.keys.include?(:x) ? dictionary[:x] : x }
+      puts tweet_text.collect { |x| dictionary.keys.include?(x.to_sym) ? dictionary[x.to_sym] : x }
   binding.pry
 end
 
