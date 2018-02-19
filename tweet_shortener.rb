59d92881f29e17bfe851288edc3f59f56a04f256
J.D. Bean <jonathandbean@gmail.com>
2018-02-19 17:28:08 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,4 +33,4 @@ def selective_tweet_shortener(tweet)
     binding.pry
 end
 
-selective_tweet_shortener("Hey guys, can anyone teach me how 2 b cool? I really want 2 b the best @ everything, u know what I mean? Tweeting is super fun u guys!!!!")
+selective_tweet_shortener("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
